ee631f525a04b89f69e7946f53243e08354f5e76
Leonardo Delmiro <leodelmiroms@gmail.com>
2022-06-28 17:14:11 -0300

    feat: include fraude detector fake logic to send message

--- service-frauddetector/src/main/java/br/com/leodelmiro/ecommerce/FraudDetectorService.java
+++ service-frauddetector/src/main/java/br/com/leodelmiro/ecommerce/FraudDetectorService.java
@@ -2,7 +2,9 @@ package br.com.leodelmiro.ecommerce;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
+import java.math.BigDecimal;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 public class FraudDetectorService {
 
@@ -18,7 +20,9 @@ public class FraudDetectorService {
         }
     }
 
-    private void parse(ConsumerRecord<String, Order> record) {
+    private final KafkaDispatcher<Order> orderKafkaDispatcher = new KafkaDispatcher<>();
+
+    private void parse(ConsumerRecord<String, Order> record) throws ExecutionException, InterruptedException {
         System.out.println("------------------------------------------");
         System.out.println("Processing new order, checking for fraud");
         System.out.println(record.key());
@@ -31,6 +35,18 @@ public class FraudDetectorService {
             //ignoring
             e.printStackTrace();
         }
-        System.out.println("Order processed");
+        var order = record.value();
+        if (isFraud(order)) {
+            //pretending that the fraud happens when amount is >= 4500
+            System.out.println("Order is a fraud!!!!!");
+            orderKafkaDispatcher.send("ECOMMERCE_ORDER_REJECTED", order.getUserId(), order);
+        } else {
+            System.out.println("Order processed");
+            orderKafkaDispatcher.send("ECOMMERCE_ORDER_APPROVED", order.getUserId(), order);
+        }
+    }
+
+    private boolean isFraud(Order order) {
+        return order.getAmount().compareTo(new BigDecimal("4500")) >= 0;
     }
 }
